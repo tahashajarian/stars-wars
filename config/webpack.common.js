4d8380b91893b0f64ed1aa41ca3560a07487e6f3
طاها شجریان <t.shajarian@pdnco>
2021-05-29 18:43:07 +0430

    change titiel and icon

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -39,8 +39,8 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      favicon: paths.src + '/images/favicon.png',
+      title: 'stars war',
+      favicon: paths.src + '/images/favicon.jpg',
       template: paths.src + '/template.html', // template file
       filename: 'index.html', // output file
     }),
